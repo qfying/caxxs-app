22126eb9a81eef6a9f714fc288b513df02911ffd
wangjinshuang <wangjinshuang@ruijie.com.cn>
2025-08-19 09:16:13 +0000

    调整

--- src/App.tsx
+++ src/App.tsx
@@ -112,12 +112,12 @@ const IonicApp: React.FC<IonicAppProps> = ({
   const [log, setLog] = useState(true);
   const { userInfo, setUserInfo } = useUserStore();
 
-  const [istoken, setistoken] = useState(true);
+  const [istoken, setistoken] = useState(false);
 
 
   useEffect(() => {
 
-    // initLogin();
+    initLogin();
     loadUserData();
     loadConfData();
 
